a9daa48188f3802d353d087759143f336605b493
ddecataldo <davidedecataldo92@gmail.com>
2021-10-07 14:09:53 +0200

    Esercizio Finito

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const nomeUtente = prompt("Inserisci il tuo nome");
+const cognomeUtente = prompt("Inserisci il tuo cognome");
+const coloreUtente = prompt("Inserisci il tuo colore preferito");
+
+alert(nomeUtente+cognomeUtente+coloreUtente+"21");
+
+/* Template literals
+alert(`${nomeUtente}${cognomeUtente}${coloreUtente}21`);
+*/
